a43fcc0443a9bd4da6bedacdb21e610617781803
Thomas Spradling <thomasspradling13@gmail.com>
2023-04-10 10:37:07 -0700

    Finished

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+  const [isBold, setIsBold] = useState(false);
+
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setIsBold(true)}>
+      {props.type}
+    </li>
+  );
+}
+
+const Strawberries = () => (<li>Strawberries</li>);
+const Cucumbers = () => (<li>Cucumbers</li>);
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => (<GroceryListItem type={item} />) )}
+  </ul>
+);
+ReactDOM.render(<GroceryList items={['Mangos', 'Strawberries', 'Cucumbers']} />, document.getElementById("app"));
